81ccac9864a16ad09c39edf34f6f858553b3905a
DING JUNYU <dd196430270@gmail.com>
2019-09-30 16:28:11 +0900

    9.30 1627 tei
    -クールダウンタイム監視と正常作動を完成し確認しました。

---
+++ Assets/Scripts/Magic/CoolDownTimeMarker.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoolDownTimeMarker : MonoBehaviour {
+    // Update is called once per frame
+    void Update() {
+        int temp_CD;
+        temp_CD = transform.parent.GetComponent<Button>().
+            ReferCD();
+
+        if (temp_CD < 0) {
+            GetComponent<Text>().text = null;
+            return;
+        }
+
+        GetComponent<Text>().text = temp_CD.ToString();
+    }
+}
