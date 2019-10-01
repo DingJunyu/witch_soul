49b5a8b2f0994018f8224854b2d8401b9c075f65
DING JUNYU <dd196430270@gmail.com>
2019-10-01 13:57:26 +0900

    10.1 1357 目次更新

--- Assets/Scripts/Controller/EnemyStatusChecker.cs
+++ Assets/Scripts/Controller/EnemyStatusChecker.cs
@@ -18,6 +18,6 @@ public class EnemyStatusChecker : MonoBehaviour {
     }
 
     private void OnCollisionEnter(Collision collision) {
-        Destroy(gameObject);
+        //Destroy(gameObject);
     }
 }
