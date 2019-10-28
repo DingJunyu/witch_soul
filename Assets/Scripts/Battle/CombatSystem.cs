5cf5fcec757a828670707c5b8ced285b91e952aa
DING JUNYU <dd196430270@gmail.com>
2019-10-28 15:42:28 +0900

    10.28 1542 update

--- Assets/Scripts/Battle/CombatSystem.cs
+++ Assets/Scripts/Battle/CombatSystem.cs
@@ -3,13 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class CombatSystem : MonoBehaviour {
-    public float pu_AttackInterval = 2f;
+    public float pu_AttackInterval = 3f;
+    private const float mc_attackDelay = 1f;
+    private float m_attackTime = 0f;
     private float m_lastAttackedTime = 0f;
 
     public float pu_damage = 1f;
 
     public void Engage() {
         m_canIAttack = true;
+        m_attackTime = Time.time;
     }
 
     public void CeaseFire() {
