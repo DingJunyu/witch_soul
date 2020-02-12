e73fa95f5b1d98b9ac59457dd409166b5c03f649
DING JUNYU <dd196430270@gmail.com>
2020-02-12 12:06:00 +0900

    2020.2.12 1.00

--- Assets/Scripts/Controller/PlayerDragPoint.cs
+++ Assets/Scripts/Controller/PlayerDragPoint.cs
@@ -4,10 +4,21 @@ using UnityEngine;
 
 public class PlayerDragPoint : MonoBehaviour {
     GameObject o_gameManager;
+    Vector3 m_myPos;
+    Vector3 m_randPos;
 
     // Start is called before the first frame update
     void Start() {
         o_gameManager = GameObject.Find("GameManager");
+        m_myPos = new Vector3(0f, 3.3f, 0f);
+        m_randPos = new Vector3(1f, 3.3f, 1f);
+    }
+
+    private void Update() {
+        //原因不明のドラッグバッグがあったが、座標をたまに変更するとこのバッグが解決できる。
+        //バッグがなくなった理由はまだ分かりません。
+        transform.localPosition = m_randPos;
+        transform.localPosition = m_myPos;
     }
 
     private void OnMouseDown() {
